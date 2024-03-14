fbec988b54b492221613db7755ec1d012a90d03b
Abdulrehman <arehmaanmughal@gmail.com>
2024-03-14 21:42:08 +0500

    products navbar

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Featured = () => {
   return (
     <div>
-      
+      Feature
     </div>
   )
 }
--- src/components/ProductsNavbar/ProductsNavbar.jsx
+++ src/components/ProductsNavbar/ProductsNavbar.jsx
@@ -1,21 +1,59 @@
-import React, { useState } from 'react'
-import { Nav, NavLink } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-import NewArrivals from '../NewArivals/NewArrivals'
+import React, { useState } from "react";
+import { Nav, NavLink } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import NewArrivals from "../NewArivals/NewArrivals";
+import Featured from "../Featured/Featured";
+import TopSelling from "../TopSelling/TopSelling";
 
 const ProductsNavbar = () => {
-    const [state, setState] = useState(false)
+  const [newArrivals, setNewArrivals] = useState(true);
+  const [featured, setFeatured] = useState(false);
+  const [topSelling, setTopSelling] = useState(false);
   return (
     <div>
-        <Nav className='py-3'>
-            <NavLink><Link to="./" onClick={()=> setState(true)}>New Arrivals</Link></NavLink>
-            <NavLink><Link>Featured</Link></NavLink>
-            <NavLink><Link>Top Selling</Link></NavLink>
-            
-        </Nav>
-        {state ? <NewArrivals/> : null}
+      <Nav className="py-3">
+        <NavLink>
+          <Link
+            to="./"
+            onClick={() => {
+              setNewArrivals(true);
+              setFeatured(false);
+              setTopSelling(false);
+            }}
+          >
+            New Arrivals
+          </Link>
+        </NavLink>
+        <NavLink>
+          <Link
+            to="./"
+            onClick={() => {
+              setNewArrivals(false);
+              setFeatured(true);
+              setTopSelling(false);
+            }}
+          >
+            Featured
+          </Link>
+        </NavLink>
+        <NavLink>
+          <Link
+            to="./"
+            onClick={() => {
+              setNewArrivals(false);
+              setFeatured(false);
+              setTopSelling(true);
+            }}
+          >
+            Top Selling
+          </Link>
+        </NavLink>
+      </Nav>
+      {newArrivals ? <NewArrivals /> : null}
+      {featured ? <Featured /> : null}
+      {topSelling ? <TopSelling /> : null}
     </div>
-  )
-}
+  );
+};
 
-export default ProductsNavbar
+export default ProductsNavbar;
