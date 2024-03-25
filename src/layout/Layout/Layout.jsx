18f2eccaefd2d2a76a8b06ce9afc25d73823d989
Abdulrehman <arehmaanmughal@gmail.com>
2024-03-25 11:22:26 +0500

    UPDATED

--- src/layout/Layout/Layout.jsx
+++ src/layout/Layout/Layout.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-// import Header from "../Header/Index.jsx"
+import Header from "../Header/Index.jsx"
 import Footer from "../Footer/Footer";
 const Layout = (props) => {
   return (
     <>
       <div>
-        {/* <Header /> */}
+        <Header />
         {props.children}
         <Footer />
       </div>
