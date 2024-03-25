a3a39ed10855c9dbb0d87978373e370916be8652
Abdulrehman <arehmaanmughal@gmail.com>
2024-03-25 11:04:45 +0500

    HHH

--- src/layout/Layout/Layout.jsx
+++ src/layout/Layout/Layout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from '../Header/index.jsx'
+import Header from '../Header/Index.jsx'
 import Footer from "../Footer/Footer";
 const Layout = (props) => {
   return (
