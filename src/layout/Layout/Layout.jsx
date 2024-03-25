98e860433b6420a9d026e26e8e9ea257fad948ec
Abdulrehman <arehmaanmughal@gmail.com>
2024-03-25 10:43:13 +0500

    vhvhv

--- src/layout/Layout/Layout.jsx
+++ src/layout/Layout/Layout.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import Header from "../Header/Header";
+import Header from '../Header/Header'
 import Footer from "../Footer/Footer";
-import { Container } from "react-bootstrap";
 const Layout = (props) => {
   return (
     <>
