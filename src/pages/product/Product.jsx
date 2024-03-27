36db099978aa0df9a3dd01653a8deb9a100fa468
Abdulrehman <arehmaanmughal@gmail.com>
2024-03-27 11:15:01 +0500

    shop now

---
+++ src/pages/product/Product.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../../layout/Layout/Layout'
+const Product = () => {
+  return (
+    <>
+        <Layout></Layout>
+    </>
+  )
+}
+
+export default Product
--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/home/Home";
 import About from "../pages/about/About";
+import Product from "../pages/product/Product";
 
 
 const Index = () => {
@@ -12,6 +13,7 @@ const Index = () => {
         
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/shopnow" element={<Product />} />
 
       </Routes>
       </BrowserRouter>
